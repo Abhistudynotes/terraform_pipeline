d063588fbb83fac120f0d12284e71d4e446de877
Abhistudynotes <abhijeetp0229@gmail.com>
2026-03-29 00:55:03 +0530

    Add Terraform configuration files

--- terraform pipeline/rg.tf
+++ terraform pipeline/rg.tf
@@ -1,18 +1,22 @@
 terraform {
   required_providers {
     azurerm = {
-
       source  = "hashicorp/azurerm"
-      version = "3.0.2"
-
+      version = "~> 3.117.0"
     }
   }
 
+  backend "azurerm" {
+    resource_group_name  = "test"
+    storage_account_name = "testdemoer"
+    container_name       = "testfile"
+    key                  = "test.terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
 
-  subscription_id = "69618a60-7b0d-4ead-bde5-911b183f9003"
+  subscription_id = "0b8a345f-5bb0-4af0-89af-77476d7c9b6f"
   features {}
 
 }
@@ -20,6 +24,6 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "rg" {
 
-  name     = "test"
+  name     = "test01"
   location = "eastus"
 }
